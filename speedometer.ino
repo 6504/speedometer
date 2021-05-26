2d23e8fb3d1c128fa3f6570bf0713e51d1b2c3cb
Liam Ryde <liamryde@gmail.com>
2021-05-26 16:30:17 -0400

    Rainbow LED loop

--- speedometer.ino
+++ speedometer.ino
@@ -26,6 +26,8 @@ CRGB stripLeds[NUM_STRIP_LEDS];
 CRGB strip2Leds[NUM_STRIP2_LEDS];
 CRGB underglowLeds[NUM_UNDERGLOW_LEDS];
 
+int boostDelay;
+
 void setup() {
   Serial.begin(9600);
   
@@ -57,7 +59,32 @@ void setup() {
 
 void loop()
 {
-  
+  for(int j = 0; j < 256; j++) {
+    for(int i = 0; i < NUM_UNDERGLOW_LEDS; i++) {
+      underglowLeds[i] = Scroll((i * 256 / NUM_UNDERGLOW_LEDS + j) % 256);      
+    } 
+
+    FastLED.show();
+    delay(100);    
+  } 
+}
+
+CRGB Scroll(int pos) {
+  CRGB color (0,0,0);
+  if(pos < 85) {
+    color.g = 0;
+    color.r = ((float)pos / 85.0f) * 255.0f;
+    color.b = 255 - color.r;
+  } else if(pos < 170) {
+    color.g = ((float)(pos - 85) / 85.0f) * 255.0f;
+    color.r = 255 - color.g;
+    color.b = 0;
+  } else if(pos < 256) {
+    color.b = ((float)(pos - 170) / 85.0f) * 255.0f;
+    color.g = 255 - color.b;
+    color.r = 1;
+  }
+  return color;
 }
 
 /* Listen for serial events from the USB connection
